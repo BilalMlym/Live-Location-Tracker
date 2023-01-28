1e4cd598af8cf0c979d538c06c237dc844d66608
Bilal Mülayim <bilalmulayim@Bilal-MacBook-Pro.local>
2023-01-29 02:11:31 +0300

    ios location data has seen

--- App.js
+++ App.js
@@ -1,18 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import * as Location from 'expo-location';
+import React, { useState, useEffect } from 'react';
+
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>{text}
+      
+      </Text>
+    
     </View>
   );
+
+
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Hello bilal</Text>
+      
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
+
 }
+
+// export default function App() {
+
+
+
+
+
+
+  
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    fontSize: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
