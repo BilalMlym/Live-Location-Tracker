7911cb4b000cb9f32a1fbc21d1faf026b6f64194
Bilal Mülayim <bilalmulayim@Bilal-MBP.home>
2023-02-03 04:22:42 +0300

    solved: axios post method working

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Platform } from 'react-native';
-import * as Location from 'expo-location';
-import React, { useState, useEffect } from 'react';
-
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ActionSheetIOS,
+  Linking,
+} from "react-native";
+import * as Location from "expo-location";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
-  const [location, setLocation] = useState(null);
+  const [location, setLocation] = useState([]);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [dt, setDt] = useState(new Date().toLocaleString());
+  const latitude = location["coords"]["latitude"];
+  const longitude = location["coords"]["longitude"];
+
+  function handleClick() {
+    axios
+      .post("https://9c7c-217-131-110-243.ngrok.io/home", {
+        latitude: latitude,
+        longitude: longitude,
+        title: "current location",
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
   useEffect(() => {
     (async () => {
-      
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
         return;
       }
 
@@ -22,49 +46,37 @@ export default function App() {
     })();
   }, []);
 
-  let text = 'Waiting..';
+  let text = "Waiting..";
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
     text = JSON.stringify(location);
   }
 
+  useEffect(() => {
+    let secTimer = setInterval(() => {
+      setDt(new Date().toLocaleString());
+    });
+    console.log(dt);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}
-      
-      </Text>
-    
+      <Button onPress={handleClick} title="Press"></Button>
+      <Text style={styles.red}>{dt}</Text>
+      <Text style={styles.red}></Text>
+
+      <Text style={styles.paragraph}>{text}</Text>
+      <StatusBar style="auto" />
     </View>
   );
-
-
-  // return (
-  //   <View style={styles.container}>
-  //     <Text>Hello bilal</Text>
-      
-  //     <StatusBar style="auto" />
-  //   </View>
-  // );
-
 }
-
-// export default function App() {
-
-
-
-
-
-
-  
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     fontSize: 10,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
