950b19653c1264f0e562e21b6dbe37677549eb6b
Bilal Mülayim <bilalmulayim@Bilal-MacBook-Pro.local>
2023-02-07 01:46:24 +0300

    working fine: fetching location data and posting to server

--- App.js
+++ App.js
@@ -1,30 +1,65 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  ActionSheetIOS,
-  Linking,
-} from "react-native";
 import * as Location from "expo-location";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import * as BackgroundFetch from "expo-background-fetch";
+import * as TaskManager from "expo-task-manager";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-  const [location, setLocation] = useState([]);
+  const [latitude, setLatitude] = useState([]);
+  const [longitude, setLongitude] = useState([]);
   const [errorMsg, setErrorMsg] = useState(null);
   const [dt, setDt] = useState(new Date().toLocaleString());
-  const latitude = location["coords"]["latitude"];
-  const longitude = location["coords"]["longitude"];
 
-  function handleClick() {
+  const ref = [];
+
+  const TASK_NAME = "BACKGROUND_TASK";
+
+  TaskManager.defineTask(TASK_NAME, () => {
+    try {
+      // fetch data here...
+      const receivedNewData = "Simulated fetch " + Math.random();
+      console.log("My task ", receivedNewData);
+      return receivedNewData
+        ? BackgroundFetch.Result.NewData
+        : BackgroundFetch.Result.NoData;
+    } catch (err) {
+      return BackgroundFetch.Result.Failed;
+    }
+  });
+
+  RegisterBackgroundTask = async () => {
+    try {
+      await BackgroundFetch.registerTaskAsync(TASK_NAME, {
+        minimumInterval: 5, // seconds,
+      });
+      console.log("Task registered");
+    } catch (err) {
+      console.log("Task Register failed:", err);
+    }
+  };
+
+  useEffect(() => {
+    ref.location = setInterval(sendLocation, 1000);
+
+    return () => {
+      if (ref.location) {
+        clearInterval(ref.location);
+      }
+    };
+  }, []);
+
+  function sendLocation() {
     axios
-      .post("https://9c7c-217-131-110-243.ngrok.io/home", {
-        latitude: latitude,
-        longitude: longitude,
-        title: "current location",
-      })
+      .post(
+        "https://ed18-31-223-44-6.ngrok.io/home",
+
+        {
+          latitude: { latitude },
+          longitude: { longitude },
+        }
+      )
       .then(function (response) {
         console.log(response);
       })
@@ -42,31 +77,40 @@ export default function App() {
       }
 
       let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+      const latitude = location["coords"]["latitude"];
+      const longitude = location["coords"]["longitude"];
+      setLatitude(latitude);
+      setLongitude(longitude);
     })();
   }, []);
 
-  let text = "Waiting..";
+  let text2 = "Waiting..";
   if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
+    text2 = errorMsg;
+  } else if (latitude) {
+    text2 = JSON.stringify(latitude);
+  }
+  let text3 = "Waiting..";
+  if (errorMsg) {
+    text3 = errorMsg;
+  } else if (longitude) {
+    text3 = JSON.stringify(longitude);
   }
 
   useEffect(() => {
     let secTimer = setInterval(() => {
       setDt(new Date().toLocaleString());
     });
-    console.log(dt);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Button onPress={handleClick} title="Press"></Button>
+      <Button onPress={sendLocation} title="SARITUTKU"></Button>
       <Text style={styles.red}>{dt}</Text>
-      <Text style={styles.red}></Text>
 
-      <Text style={styles.paragraph}>{text}</Text>
+      {/* <Text style={styles.paragraph}> {text}</Text> */}
+      <Text style={styles.paragraph}>latitude: {text2}</Text>
+      <Text style={styles.paragraph}>longitude: {text3}</Text>
       <StatusBar style="auto" />
     </View>
   );
